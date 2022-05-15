c2ecc221c33dd3cce5c9576aa3f26be22d7ba4d9
zafarfed <zafarstyle@mail.ru>
2022-05-16 01:29:43 +0500

    u-sh

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { Header, P} from './style';
+import {Routes, Route, useNavigate} from 'react-router-dom'
 
 function App() {
+
+  const navigate = useNavigate()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+
+    <Header>
+
+      <P onClick={()=>navigate('/')}>Home</P>
+      <P onClick={()=>navigate('/about')}>About</P>
+      <P onClick={()=>navigate('/signup')}>Sigh Up</P>
+      <P onClick={()=>navigate('/register')}>Register</P>
+
+
+                
+    </Header>
+        <Routes>
+            <Route path='/' element={'Home'}/>
+            <Route path='/signup' element={'Signup'}/>
+            <Route path='/about' element={'About'}/>
+            <Route path='/register' element={'Register'}/>
+            <Route path='*' element={'404 Not found'}/>
+        </Routes> 
     </div>
   );
 }
---
+++ src/style.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const Header = styled.div`
+width: 100%;
+height: 60px;
+display: flex;
+justify-content: space-around;
+background-color: darkgreen;
+padding: 10px 0;
+align-items: center;
+`
+
+export const P = styled.p`
+color: wheat;
+font-size: 20px;
+font-weight: bold;
+cursor: pointer;
+`
